c763b936982b020e7d248cdd2b8ace4f6265e40b
Diego Moreira Sardinha de Souza <diegoaguia31@gmail.com>
2021-08-28 16:50:48 -0300

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+jQuery(function ($) {
+
+  $(".answer").css("display", "none");
+
+
+  $(".question").click(function () {
+
+    $(".question").not(this).removeClass("open");
+
+    $(".question").not(this).next().slideUp(300);
+
+    $(this).toggleClass("open");
+
+    $(this).next().slideToggle(300);
+  });
+});
